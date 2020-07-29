657e90cdbb92ae7534d39b55efb11bf8ffff066a
Andriy Bashuk <abashuk@newagesol.com>
2020-07-29 15:38:35 +0300

    init commit

---
+++ src/ServiceProvider.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Spartaques\LaravelPinba;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $configPath = __DIR__ . '/../config/pinba.php';
+        $this->mergeConfigFrom($configPath, 'pinba');
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+         __DIR__ . '/../config/pinba.php' => config_path('pinba.php')
+        ]);
+
+
+        if (extension_loaded('pinba')) {
+
+            if (ini_get ('pinba.enabled')) {
+
+                pinba_hostname_set(config('app.name'));
+            }
+
+            if (config('pinba.enabled')) {
+
+                    ini_set('pinba.enabled', config('pinba.enabled'));
+
+                if (ini_get('pinba.server') === '') {
+                    ini_set('pinba.server', config('pinba.server'));
+                }
+
+                if(app()->runningInConsole()) {
+                    pinba_schema_set('console');
+                } else {
+                    pinba_schema_set('web');
+                }
+
+                /** @var \Illuminate\Routing\Router $router */
+                $router = $this->app['router'];
+                $router->middleware('pinba', Middleware::class);
+
+                /** @var \Illuminate\Foundation\Http\Kernel $kernel */
+                $kernel = $this->app['Illuminate\Contracts\Http\Kernel'];
+                $kernel->pushMiddleware(Middleware::class);
+            } else {
+                ini_set('pinba.enabled', false);
+            }
+        }
+
+        $this->app->singleton(LaravelPinba::class, function () {
+            return new LaravelPinba();
+        });
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            LaravelPinba::class
+        ];
+    }
+}
